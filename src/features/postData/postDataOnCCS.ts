8590f63c053f6a3510e2f5cb65ebedcd8ab52605
Andy <workoanda@gmail.com>
2024-05-02 15:56:31 -0300

    fix postDataOnCCS

--- src/features/postData/postDataOnCCS.ts
+++ src/features/postData/postDataOnCCS.ts
@@ -2,12 +2,12 @@ import { environmentService } from '@core/environment';
 import { ISheetsData } from 'src/interfaces/sheetsData.interface';
 
 export function postDataOnCCS(sheetsData: ISheetsData) {
-  const controlUrl = environmentService.get('CONTROL_URL');
-  const controlSS = SpreadsheetApp.openByUrl(controlUrl);
+  const sheetUrl = environmentService.get('SHEET_URL');
+  const sheetSS = SpreadsheetApp.openByUrl(sheetUrl);
   const sheetsNames = environmentService.sheetsNames;
 
   sheetsNames.forEach((name) => {
-    const activeSheet = controlSS.getSheetByName(name);
+    const activeSheet = sheetSS.getSheetByName(name);
     const activeData = sheetsData[name];
     for (let i = 1; i < activeData.length; i++) {
       activeSheet?.getRange(i + 1, 1, 1, 9).setValues([activeData[i]]);
