3f45731880e491fb44cbda797430267cf07b93ba
Andy <workoanda@gmail.com>
2024-05-06 09:29:48 -0300

    update environment.service

--- src/core/environment/environment.service.ts
+++ src/core/environment/environment.service.ts
@@ -1,26 +1,5 @@
 import { EnvironmentService } from './environment.type';
 
-/**
- * Environment variables usage approach
- *
- * ---
- *
- * You can use an EnvironmentService in order to
- * avoid repeat the `process.env` pattern.
- *
- * You are free to implement this in any way you
- * want, but this is a good starting point.
- *
- * I recommend you to make extensive usage of
- * generics when implementing a single environment
- * variable getter method. Like so:
- *
- * ```typescript
- * get<Key extends keyof Environment>(key: Key): Environment[Key] {
- *   // Implementation...
- * }
- * ```
- */
 export const environmentService: EnvironmentService = {
   get(key) {
     const environment = {
