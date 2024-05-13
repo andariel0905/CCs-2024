ec4883fb5a5381ef7b231b23fb525b800b3d3bc3
Andy <workoanda@gmail.com>
2024-05-13 12:49:52 -0300

    develop postDataOnCCS service

--- src/features/postData/postDataOnCCS.ts
+++ src/features/postData/postDataOnCCS.ts
@@ -2,9 +2,13 @@ import { environmentService } from '@core/environment';
 import { ISheetsData } from 'src/interfaces/sheetsData.interface';
 
 export function postDataOnCCS(sheetsData: ISheetsData) {
-  const sheetUrl = environmentService.get('SHEET_URL');
-  const sheetSS = SpreadsheetApp.openByUrl(sheetUrl);
+  const CSSUrl = environmentService.get('SHEET_URL');
+  const CSS_SS = SpreadsheetApp.openByUrl(CSSUrl);
   const sheetsNames = environmentService.sheetsNames;
-
-  console.log(sheetsData, sheetSS, sheetsNames);
+  for (const sheetName of sheetsNames) {
+    const activeSheet = CSS_SS.getSheetByName(sheetName);
+    const lastRow = activeSheet?.getLastRow();
+    const activeData = [...sheetsData[sheetName].values()];
+    activeSheet?.getRange('A2:L' + lastRow).setValues(activeData);
+  }
 }
